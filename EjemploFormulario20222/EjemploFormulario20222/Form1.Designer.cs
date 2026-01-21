b69a3ce9cc1869de810cc2403ed5212ba31cbcc7
Maria Camila Castaño M. <82916777+camilacastano@users.noreply.github.com>
2026-01-21 12:11:41 -0500

    Add files via upload

---
+++ EjemploFormulario20222/EjemploFormulario20222/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace EjemploFormulario20222
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIngresarEdad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIngresarEdad
+            // 
+            this.lblIngresarEdad.AutoSize = true;
+            this.lblIngresarEdad.Location = new System.Drawing.Point(90, 88);
+            this.lblIngresarEdad.Name = "lblIngresarEdad";
+            this.lblIngresarEdad.Size = new System.Drawing.Size(185, 25);
+            this.lblIngresarEdad.TabIndex = 0;
+            this.lblIngresarEdad.Text = "Ingrese una edad:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(281, 85);
+            this.txtEdad.MaxLength = 5;
+            this.txtEdad.Multiline = true;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(368, 232);
+            this.txtEdad.TabIndex = 2;
+            this.txtEdad.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(228, 268);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 25);
+            this.lblMensaje.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(180, 160);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(238, 56);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            this.btnCalcular.MouseEnter += new System.EventHandler(this.bntCalcular_MouseEnter);
+            this.btnCalcular.MouseLeave += new System.EventHandler(this.bntCalcular_MouseLeave);
+            this.btnCalcular.MouseHover += new System.EventHandler(this.bntCalcular_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(792, 430);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblIngresarEdad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIngresarEdad;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ EjemploFormulario20222/EjemploFormulario20222/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EjemploFormulario20222
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+            float
+            int edad;
+            if (txtEdad.Text != string.Empty)
+            {
+                edad = Convert.ToInt32(txtEdad.Text);
+                if (edad >= 18)
+                {
+                    lblMensaje.Text = "La persona es mayor de edad.";
+                }
+                else
+                {
+                    lblMensaje.Text = "La persona es menor de edad.";
+                }
+            }
+            else
+            {
+                lblMensaje.Text = "Debe ingresar informacion.";
+            }
+        }
+
+        private void bntCalcular_MouseHover(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void bntCalcular_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        private void bntCalcular_MouseEnter(object sender, EventArgs e)
+        {
+            //Random numAleatorio = new Random();
+            //btnCalcular.Location = new Point(numAleatorio.Next(0,300), 100);
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEdad.Text != string.Empty)
+                btnCalcular.Enabled = true;
+            else
+                btnCalcular.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnCalcular.Enabled = false;
+        }
+    }
+}
